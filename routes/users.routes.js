5407e689d543533362644a2940888b97f401fd8a
diegomolina83 <diegomolina83@gmail.com>
2020-09-19 16:35:14 +0200

    0.32

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const User = require('../models/user.model')
 const router = express.Router()
+const cdnUploader = require('../configs/cloudinary.config')
+const Picture = require('../models/picture.model')
+
+
+
+
+//Listado de usuarios
 
 router.get('/users', (req, res) => {
 
@@ -11,6 +18,49 @@ User.find()
 })
 
 
+//Borrar usuarios
+
+router.get('/users/delete/:id',(req,res)=>{
+    const id = req.params.id
+    User.findByIdAndDelete(id)
+    .then(()=>res.redirect('/users'))
+    .catch(err=>console.log('Error: ', err))
+})
+
+
+//Editar usuarios
+
+router.get('/users/edit/:id',(req,res)=>{
+    const id = req.params.id
+        User.findByIdAndUpdate(id)
+        .then(user=>res.render('users/users-edit',user))
+        .catch(err=>console.log('Error: ', err))
+})
+
+router.post('/users/edit/:id',cdnUploader.single('imageInput'),(req,res)=>{
+    const id = req.params.id
+    const {username,email,imageInput,password} = req.body
+
+
+    if (req.file) {
+
+        if(req.file.size>2000000){
+            res.render("auth/signup", { errorMsg: "El tamaño de la imagen no puede ser superior a 2 MB" })
+            return
+        } 
+
+        Picture.create({
+            name:req.file.originalname,
+            path:req.file.path,
+            originalName: req.file.originalname
+            })
+            imageUrl= req.file.path
+      }
+
+    User.findByIdAndUpdate(id,{username,email,imageUrl,password})
+    .then(()=>{res.redirect('/users')})
+    .catch(err=>console.log('Error: ', err))
+})
 
 
 module.exports = router
