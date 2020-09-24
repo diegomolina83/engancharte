1ff7718818b09a6e552a835220a12f2cde21c748
Pedro Conde <condepedro968@gmail.com>
2020-09-24 20:43:36 +0200

    adding changes

--- configs/debugger.config.js
+++ configs/debugger.config.js
@@ -3,4 +3,4 @@ const path = require('path')
 const app_name = require('../package.json').name;
 //const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`)
 
-module.exports = debug
+//module.exports = debug
